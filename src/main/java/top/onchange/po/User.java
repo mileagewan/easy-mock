0860d2ff303c1e56acb51e29243b2ba97910df56
WLC-PC\WLC <792668520@qq.com>
2020-11-01 16:19:54 +0800

    【story】：新开发 【autor】：mileage

---
+++ src/main/java/top/onchange/po/User.java
@@ -0,0 +1,13 @@
+package top.onchange.po;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User {
+    private int id;
+    private String userName;
+    private String passWord;
+    private String realName;
+}
